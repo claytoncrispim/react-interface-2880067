a5f5eace81020a993d3a8265a8c3d45d8b41ddfb
Clayton Crispim <43951382+claytoncrispim@users.noreply.github.com>
2025-09-05 16:33:46 +0000

    feat: Implement appointment sorting interface
    - Added a dropdown menu to the Search component for sorting options.
    
    - Users can now sort the appointment list by Pet Name, Owner Name, or Date.
    
    - Implemented logic to handle both ascending and descending order.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
+import { useState, useEffect, useCallback  } from "react";
 import { BiCalendar } from "react-icons/bi";
-import { BiTrash } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appointmentList from "./data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  // State to hold the list of appointments
+  let [ appointmentList, setAppointmentList ] = useState([]); // Initial value is an empty array
+  let [ query, setQuery ] = useState(''); // State to hold the search query
+  let [ sortBy, setSortBy ] = useState('petName'); // State to initialize by petName by default
+  let [ orderBy, setOrderBy ] = useState('asc'); // State to initialize by ascending order by default
+
+  const filteredAppointments = appointmentList.filter(
+    // 1. Convert both the appointment fields and the query to lowercase to make the search case-insensitive
+    // 2. Check if the query is included in any of the fields: petName, ownerName, or aptNotes
+    item => {
+      return (
+        item.petName.toLowerCase().includes(query.toLowerCase()) ||
+        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+      )
+    }
+  ).sort((a, b) => {
+    let order = (orderBy === 'asc') ? 1 : -1; // Determine the order based on the orderBy state
+    return (
+      // Control how the two items are compared
+      // 1. If a is less than b, return -1 multiplied by the order (1 for ascending, -1 for descending)
+      // 2. If a is greater than b, return 1 multiplied by the order
+      // This way, the sorting will be done in the specified order (ascending or descending)
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase()
+        ? -1 * order : 1 * order
+    )
+  })
+
+  // Fetch data from the JSON file
+  const fetchData = useCallback(() => { // useCallback to monitor any changes that happen in the data
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data);
+      })
+  }, [])
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]) // Asking useEffect to track the fetching of data
+
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3">
@@ -12,24 +53,37 @@ function App() {
       </h1>
       {/* Components */}
       <AddAppointment />
-      <Search />
+      <Search query={query}
+        onQueryChange={myQuery => setQuery(myQuery)}
+        orderBy={orderBy}
+        onOrderByChange={myOrder => setOrderBy(myOrder)}
+        sortBy={sortBy}
+        onSortByChange={mySort => setSortBy(mySort)}
+      />
 
       <ul className="divide-y divide-gray-200">
-        {appointmentList.map((appointment) => 
-          <li className="px-3 py-3 flex items-start">
-            <button type="button"
-              className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-              <BiTrash /></button>
-            <div className="flex-grow">
-              <div className="flex items-center">
-                <span className="flex-none font-medium text-2xl text-blue-500">{appointment.petName}</span>
-              <span className="flex-grow text-right">{appointment.aptDate}</span>
-              </div>
-              <div><b className="font-bold text-blue-500">Owner:</b> {appointment.ownerName}</div>
-              <div className="leading-tight">{appointment.aptNotes}</div>
-            </div>
-          </li>
-        )}
+        {/* Replacing appointmentList with filteredAppointments to show the filtered list */}
+        {/* {appointmentList */}
+        {filteredAppointments
+          .map(appointment => ( 
+            // Everytime you loop through a serie of elements, we need to use a key
+            <AppointmentInfo key={appointment.id}
+              // Passing down the whole appointment object as a prop in an "appointment" variable
+              appointment={appointment}
+              // Passing down the setAppointmentList method that we created above and is
+              // part of useState to delete an appointment
+              // 1. We create an arrow function that receives the appointmentId
+              // 2. We call the setAppointmentList method to update the state
+              // 3. We filter the appointmentList to remove the appointment with the matching id
+              // 4. The filtered list is then set as the new state
+              onDeleteAppointment={ 
+                appointmentId => 
+                  setAppointmentList(appointmentList.filter(appointment => 
+                    appointment.id !== appointmentId))
+                }
+            />
+          ))
+        }
       </ul>
     </div>
   );
--- src/components/AddAppointment.js
+++ src/components/AddAppointment.js
@@ -1,12 +1,21 @@
 import { BiCalendarPlus } from "react-icons/bi";
+import { useState } from "react";
 
 const AddAppointment = () => {
+    // useState is a hook that allows us to have state variables in functional components by importing it from 
+    // react and calling it inside the component function body. It is used to create state variables that can hold 
+    // and manage data that may change over time.
+    // In this case, toggleForm is a boolean state variable that indicates whether the form is visible or not.
+    let [toggleForm, setToggleForm] = useState(false); // State to show/hide the form
     return (
         <div>
-            <button className="bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md">
+            <button onClick={() => setToggleForm(!toggleForm)}
+                className={`bg-blue-400 text-white px-2 py-3 w-full text-left rounded-t-md ${toggleForm ? 'rounded-t-md' : 'rounded-md'}`}>
                 <div><BiCalendarPlus className="inline-block align-text-top" />  Add Appointment</div>
             </button>
-            <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
+            {
+                toggleForm &&
+                            <div className="border-r-2 border-b-2 border-l-2 border-light-blue-500 rounded-b-md pl-4 pr-4 pb-4">
                 <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start  sm:pt-5">
                     <label htmlFor="ownerName" className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">
                         Owner Name
@@ -68,6 +77,8 @@ const AddAppointment = () => {
                     </div>
                 </div>
             </div>
+            }
+
         </div>        
     )
 }
